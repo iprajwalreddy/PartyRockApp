6cea1bdeecab17036f9840415518302832ecf558
SaiReddy <saireddy@SaiReddys-iMac.local>
2017-02-13 22:56:17 +0530

    Just finished the basic UI

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by SaiReddy on 29/01/17.
+//  Copyright © 2017 SaiReddy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+
+
+}
+
